565362acdba45e6c6f32429150aa02147407b5d4
sudheeshrama <sudheeshcr099@gmail.com>
2019-06-02 12:32:46 +0530

    titlechange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Router } from "@angular/router";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "oshop";
+  title = "guruprasad";
 
   constructor(
     private userService: UserService,
